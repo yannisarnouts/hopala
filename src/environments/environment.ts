a133feeed1282b708136a18fec985a04fcceed4d
yannisarnouts <yarnouts@gmail.com>
2019-07-19 17:06:19 +0200

    werkend

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyAsw3pQi53uNJU2a07rAQcI3rsB0zceXnw',
+    authDomain: 'hopalaapp.firebaseapp.com',
+    databaseURL: 'https://hopalaapp.firebaseio.com',
+    projectId: 'hopalaapp',
+    storageBucket: '',
+    messagingSenderId: '776001184620',
+    appId: '1:776001184620:web:72dd9a02d480e17f'
+  }
+}
